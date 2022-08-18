cca125879a22d481966fb847e3039c13f3533257
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-08-18 05:09:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+
+//const kompanyName = "Scuber";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
